41e80bbae86ff59daa0c027fbdccb7fcfe3dfc25
Satosh <smartdev.house108@gmail.com>
2023-03-13 09:07:31 -0500

    Add http client and define request for shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,16 @@
 import { Product } from "./products";
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = [];
-/* . . . */
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,5 +24,7 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-/* . . . */
+  getShippingPrices() {
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
+  }
 }
